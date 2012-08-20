56e6c7d4de0fa817028c92e667a206db1562db53
Boguslaw Tolarz <btolarz@gmail.com>
2012-08-20 14:11:42 +0200

    Method name changed

--- lib/google/calendar.rb
+++ lib/google/calendar.rb
@@ -163,7 +163,7 @@ module Google
       calendar_data.xpath("//entry[title='#{@calendar}']/color/@value").first.value
     end
 
-    def caldendar_name
+    def name
       calendar_data.xpath("//entry[id='http://www.google.com/calendar/feeds/default/allcalendars/full/#{@calendar.gsub("@","%40")}']/title").text
     end
 
